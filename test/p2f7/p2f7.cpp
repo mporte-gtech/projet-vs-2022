5e2ad3c7c43799022d7277a7c2def12d72cd664d
PorteMaxime <mporte@gaming.tech>
2025-10-15 15:48:20 +0200

    changements

--- test/p2f7/p2f7.cpp
+++ test/p2f7/p2f7.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-int* GetSecretWordIndex();
-bool IsIndexUsed();
-AddUsedIndex();
+int GetSecretWordIndex(std::string*, int* guessedWords, int wordsAmount);
+bool IsIndexUsed(int index, int* guessedWords);
+void AddUsedIndex(int* usedWordsList, int usedWordIndex);
 void GetPlayerGuess();
-void CompareWords();
+bool CompareWords(std::string* guessAttempt, std::string* secretWord, int wordLength);
 void PlayOneWord();
 
 int main()
@@ -12,31 +12,93 @@ int main()
 	const int wordLength = 5;
 	const int maxTryAmount = 6;
 	std::string availableWords[] = {"pomme", "table", "livre", "chaud", "arbre"};
-	int* foundWords = (int*)malloc(sizeof(int) * (sizeof(availableWords) / sizeof(*availableWords)));
+	int* guessedWordsIndexes = (int*)malloc(sizeof(int) * (sizeof(availableWords) / sizeof(*availableWords)));
+	int wordsAmount = sizeof(availableWords) / sizeof(*availableWords);
 
 	srand(time(NULL));
-	GetSecretWordIndex
-
+	int secretWordIndex = GetSecretWordIndex(availableWords, guessedWordsIndexes, wordsAmount);
 
 }
 
-int* GetSecretWordIndex()
+int GetSecretWordIndex(std::string* wordsToGuess, int* guessedWords, int wordsAmount)
 {
-	int isWordAvailable = 0;
+	int wordToGuessIndex = NULL;
+	int remainingWords = 0;
+	
+	for (int a = 0; a < wordsAmount; a++)
+	{
+		if (guessedWords[a] == 0)
+		{
+			remainingWords++;
+		}
+	}
+
+	if (remainingWords == 0)
+		return -1;
+
+
 	do
 	{
+		wordToGuessIndex = rand() % (0, wordsAmount);
+		if (IsIndexUsed(wordToGuessIndex, guessedWords))
+		{
+				wordToGuessIndex = NULL;
+		}
+	} while (wordToGuessIndex == NULL);
 
-	} while (1==0);
-	//return rand() % (max - min + 1) + min;
-
+	return wordToGuessIndex;
 }
 
-bool IsIndexUsed(int index, int* table)
+bool IsIndexUsed(int index, int* guessedWords)
 {
-	for (size_t i = 0; i < length; i++)
+	if (guessedWords[index] == 0)
 	{
-
+		return false;
 	}
+	return true;
+}
+
+void AddUsedIndex(int* usedWordsList, int usedWordIndex)
+{
+	usedWordsList[usedWordIndex] = 1;
+}
+
+bool CompareWords(std::string* guessAttempt, std::string* secretWord, int wordLength)
+{
+	int guessedLetters = 0;
+
+	for (int i = 0; i < wordLength; i++)
+	{
+		bool rightLetter = guessAttempt[i] == secretWord[i];
+		if (rightLetter)
+			guessedLetters++;
+
+		if (rightLetter)
+		{
+			// affiche vert
+		}
+		else
+		{
+			for (i = 0; i < strlen(guessAttempt); i++)
+			{
+				if (foundalpha = isalpha(instring[i]))
+				{
+					break;
+				}
+			}
+
+			if ()
+			{
+				// affiche jaune
+			}
+			else
+			{
+				// affiche rouge
+			}
+		}
+	}
+
+	return guessedLetters == wordLength ? true: false;
 }
 
 
